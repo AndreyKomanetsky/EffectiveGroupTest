a670bcefa2e1107d474b92709acc41ede026aa5e
AndreyKomanetsky <74355164+AndreyKomanetsky@users.noreply.github.com>
2021-02-17 11:39:32 +0300

    final cut

--- Generator.php
+++ Generator.php
@@ -1,36 +1,63 @@
-<?
-class Fig {
-		protected $s;
+<?php
+namespace Vendor\Model;
+	
+    class Fig 
+	{// Определяем родительский класс Fig с общимb для всех наследников полями - $s - площадью и Cheker - функцией проверки существования идентичной фигуры
+        protected $s;
+        protected function Cheker($filename, $text)
+		{ //Проверяет, была ли такая фигура сгенерирована ранее и если не была, то сохраняет файл с данными о фигруе в  
+        //каталог collection
+        //кнопка возврата на главную страницу
+        echo <<<DT
+        <form action="index.php" method="POST">
+        <input name="Back" type="submit" value="BACK" />
+        </form>
+DT;
+            if (file_exists('collection/'.$filename.'.txt')) {
+                echo 'Generated figure already exist. Come back and try again.';
+                return false;
+            } else {
+				$fp = fopen('collection/'.$filename.".txt", "w");
+				fwrite($fp, $text);
+				fclose($fp);
+				return true;
+
+            }
+        }
 	}
-	class Rectangle extends Fig {
+	class Rectangle extends Fig 
+	{// определяем класс Прямоугольника, наследником класса Fig. Поля: длинна - $length, ширина - $width, конструктор, функция вычисления  
+	//площади - RecSquare, функция вывода на экран сгенерированных параметров - PrintParam
+	
 		public $length, $width;
 		
-		public function __construct($lng, $wdt) {
+		public function __construct($lng, $wdt)
+		{ //конструктор принимает значение длинны и ширины и вызывает функцию вычисления площади
 			$this->length = $lng;
 			$this->width = $wdt;
 			$this->RecSquare();
 		}
-		private function RecSquare(){
+		private function RecSquare()
+		{//функция вычисляет площадь,вызывает функцию проверки существования идентичной фигуры, вызывает функцию вывода параметров на экран
 			$this->s = $this->length * $this->width;
 			$filename = 'rectangle-'.$this->length.'-'.$this->width;
-			if (file_exists('collection/'.$filename)){
-				echo 'Generated rectangle already exist. Come back and try again.';
-			} else {
-				$text = 'Rectangle||Length='.$this->length.' Width='.$this->width.'||'.$this->s;
-				$fp = fopen('collection/'.$filename.".txt", "w");
-				fwrite($fp, $text);
-				fclose($fp);
+			$text = 'Rectangle||Length='.$this->length.' Width='.$this->width.'||'.$this->s;
+			if ($this->Cheker($filename, $text)) {
 				$this->PrintParam();
 			}
 		}
-		private function PrintParam(){
+		private function PrintParam()
+		{//выводит на экран длину, ширину и полученную площадь
 			echo '<br>Length:'. $this->length .' , Width: '. $this->width .' , Square: '. $this->s .' <br> Rectangle was saved. <br>';
 		}
 	}
-	class Triangle extends Fig {
+	class Triangle extends Fig 
+	{//определяем класс Треугольника, наследником класса Fig. Поля: Сторона А - $aSide, Сторона B - $bSide, Сторона С - $cSide,
+	//угол между A u B - $angle, конструктор, функция нахождения площади - TriSquare,  функция вывода на экран сгенерированных параметров - PrintParam
 		public $aSide, $bSide, $cSide, $angle;
 		
-		public function __construct($aS, $bS, $ang){
+		public function __construct($aS, $bS, $ang)
+		{//Конструктор получает данные о стороне А, B и угле между ними, затем вычисляет сторону C, далее вызывет функцию TriSquare
 			$this->aSide = $aS;
 			$this->bSide = $bS;
 			$this->angle = $ang;
@@ -38,38 +65,45 @@ class Fig {
 			$this->TriSquare();
 		}
 		
-		private function TriSquare(){
+		private function TriSquare()
+		{//функция вычисляет площадь, вызывает функцию проверки существования идентичной фигуры, вызывает функцию вывода параметров на экран 
 			$p = ($this->aSide + $this->bSide + $this->cSide)/2;
 			$this->s = sqrt($p*($p-$this->aSide)*($p-$this->bSide)*($p-$this->cSide));
 			$filename = 'triangle-'.$this->aSide.'-'.$this->bSide.'-'.round($this->cSide, 3);
-			if (file_exists('collection/'.$filename)){
-				echo 'Generated triangle already exist. Come back and try again.';
-			} else {
-				$text = 'Triangle||Side A='.$this->aSide.' Side B='.$this->bSide.' Side C='.round($this->cSide, 3).'||'.round($this->s, 3);
-				$fp = fopen('collection/'.$filename.".txt", "w");
-				fwrite($fp, $text);
-				fclose($fp);
+			$text = 'Triangle||Side A='.$this->aSide.' Side B='.$this->bSide.' Side C='.round($this->cSide, 3).'||'.round($this->s, 3);
+			if ($this->Cheker($filename, $text)) {
 				$this->PrintParam();
-			} 	
+			}			
 		}
-		private function PrintParam(){
+		private function PrintParam()
+		{ //выводит на экран длину, ширину и полученную площадь
 			echo '<br> A-side: '. $this->aSide .' B-side: '. $this->bSide .' C-side: '. round($this->cSide, 3) .' Sqare: '. round($this->s, 3) .'<br> Triangle was saved. <br>';
 		}
 	}
-	class Circle extends Fig {
+	class Circle extends Fig 
+	{ //определяем класс Круга, наследников класса Fig. Поля: радиус - $radius, конструктор, функция вычисления площади - CirSquare, 
+	//функция вывода параметров на экран
 		public $radius;
-		public function __construct($r){
+		public function __construct($r)
+		{//получает радиус и вызывает функцию вычисления площади
 			$this->radius = $r;
 			$this->CirSquare();
 		}
-		private function CirSquare(){
+		private function CirSquare()
+		{//функция вычисляет площадь, вызывает функцию проверки существования идентичной фигуры, вызывает функцию вывода параметров на экран
 			$this->s = pi() * $this->radius * $this->radius;
-			$this->PrintParam();
+			$filename = 'circle-'.$this->radius;
+			$text = 'Circle||Radius='.$this->radius.'||'.round($this->s, 3);
+			if ($this->Cheker($filename, $text)) {
+				$this->PrintParam();
+			}
 		}
-		private function PrintParam(){
-			echo '<br> Radius: '. $this->radius .' Sqare: '. round($this->s, 3) .'<br>';
+		private function PrintParam()
+		{//выводит на экран длину, ширину и полученную площадь
+			echo '<br> Radius: '. $this->radius .' Sqare: '. round($this->s, 3) .'<br>Circle was saved. <br>';
 		}
 	}
+//в зависимости от нажатой кнопки создаётся одна из трёх фигур
 if (isset($_POST['GenRec'])){
 	$rect = new Rectangle(rand(1, 20), rand(1, 20));
 }
@@ -79,4 +113,3 @@ if (isset($_POST['GenTri'])){
 if (isset($_POST['GenCir'])){
 	$circ = new Circle(rand(1, 10));
 }
-?>
--- index.php
+++ index.php
@@ -1,87 +1,61 @@
-﻿<HTML>
+<HTML>
 <HEAD>
 <TITLE>PHP TEST</TITLE>
 </HEAD>
 <BODY>
-<?
-	class Fig {
-		protected $s;
-	}
-	class Rectangle extends Fig {
-		public $length, $width;
-		
-		public function __construct($lng, $wdt) {
-			$this->length = $lng;
-			$this->width = $wdt;
-			$this->RecSquare();
-		}
-		private function RecSquare(){
-			$this->s = $this->length * $this->width;
-			$this->PrintParam();
-		}
-		private function PrintParam(){
-			echo '<br>Length:'. $this->length .' , Width: '. $this->width .' , Square: '. $this->s .' <br> ';
-		}
-	}
-	class Triangle extends Fig {
-		public $aSide, $bSide, $cSide, $angle;
-		
-		public function __construct($aS, $bS, $ang){
-			$this->aSide = $aS;
-			$this->bSide = $bS;
-			$this->angle = $ang;
-			$this->cSide = sqrt($aS*$aS + $bS*$bS - 2*$aS*$bS*cos(Deg2rad($ang)));
-			$this->TriSquare();
-		}
-		
-		private function TriSquare(){
-			$p = ($this->aSide + $this->bSide + $this->cSide)/2;
-			$this->s = sqrt($p*($p-$this->aSide)*($p-$this->bSide)*($p-$this->cSide));
-			$this->PrintParam();
-		}
-		private function PrintParam(){
-			echo '<br> A-side: '. $this->aSide .' B-side: '. $this->bSide .' C-side: '. round($this->cSide, 3) .' Sqare: '. round($this->s, 3) .'<br>';
-		}
-	}
-	class Circle extends Fig {
-		public $radius;
-		public function __construct($r){
-			$this->radius = $r;
-			$this->CirSquare();
-		}
-		private function CirSquare(){
-			$this->s = pi() * $this->radius * $this->radius;
-			$this->PrintParam();
-		}
-		private function PrintParam(){
-			echo '<br> Radius: '. $this->radius .' Sqare: '. round($this->s, 3) .'<br>';
-		}
-	}
-	echo 'Задание 1. Последовательность Фибоначчи: <br>';
-	$pp = 0;
+<?php
+	
+	echo 'Task 1. Fibonacci sequence: <br>';
+	$pp = 0;//Задаём первые два элемента последовательности
 	$p = 1;
 	echo " $pp, $p ";
-	for ($i = 0; $i<62; $i++){
+	for ($i = 0; $i<62; $i++){// в цикле находим следующее число, складывая два предидущих, выводим на экран и меняем значения на новые 
 		$n=$p+$pp;
 		echo ", $n ";
 		$pp = $p;
 		$p = $n;
 	}
-	echo '<br>Задание 3:<br>';
+	echo '<br>Task 3:<br>';
+	echo 'Collection of figures:<br>';
 	echo <<<DT
-	<form action="Generator.php" method="POST">
+<table align="left" bgcolor="white" border=1px bordercolor="blue" cellpadding=3px>
+	<tr>
+		<td>Figure type</td><td>Params of figure</td><td>Square</td>
+	</tr>
+DT;
+	$files = scandir('./collection');//сканируем директорию с фигурами
+	for ($i=2; $i<count($files); $i++){//в цикле по имени файла заносим данные о фигурах в массив
+		$name = $files[$i];
+		$arrOfFigs[] = explode('||',file_get_contents('collection/'.$name));
+	}
+	if ($arrOfFigs != null) {//проверяем, есть ли сгенерированные фигуры, если таких нет, то выводим сообщение об этом
+		usort($arrOfFigs, function($a, $b) { //сортируем массив по плащади фигур, выставляем их по убыванию
+			return ($a[2] > $b[2]) ? -1 : 1; 
+			});
+		foreach ($arrOfFigs as &$value){//заполняем таблицу данными
+			echo <<<DT
+			<tr> <td>$value[0]</td><td>$value[1]</td><td>$value[2]</td> </tr>
+DT;
+		}
+	} else {
+    echo '<tr><td>No figures</td> </tr>';
+	}
+	echo '</table>';//делаем кнопки перехода на скрипт генерации фигур Generator.php. Для каждого вида фигур своя кнопка
+	echo <<<DT
+	<form action="generator.php" method="POST">
      <input name="GenRec" type="submit" value="Generate rectangle" />
-	</form> <br>
+	</form>
 DT;
 	echo <<<DT
-	<form action="Generator.php" method="POST">
+	<form action="generator.php" method="POST">
      <input name="GenTri" type="submit" value="Generate triangle" />
 	</form>
 DT;
-	#$rect = new Rectangle(rand(1, 20), rand(1, 20));
-	#$tria = new Triangle(rand(1, 20), rand(1, 20), rand(15, 165));
-	#$circ = new Circle(rand(1, 20));
-	
+	echo <<<DT
+	<form action="generator.php" method="POST">
+     <input name="GenCir" type="submit" value="Generate Circle" />
+	</form>
+DT;
 	
 ?>
 </BODY>
